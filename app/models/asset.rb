ff45bf126f4d80eb601feb9b7f8a1707076543ff
quest <quest@questmbp.local>
2009-09-15 14:23:18 -0700

    Lateest video changes

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,4 @@
 class Asset < ActiveRecord::Base
   belongs_to :owner
+  validates_presence_of :title
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,14 @@
+require 'rvideo'
+
 class Video < Asset
   has_attached_file :file
+  has_attached_file :preview
+  validates_attachment_presence :file
+  validates_attachment_content_type :file, :content_type => /video/
+  
+  def create_preview
+    # Make sure that /usr/local/bin is always in the path.
+    ENV['PATH'] = '/usr/local/bin:' + ENV['PATH']
+    frame = RVideo::FrameCapturer.capture! :input => "#{self.file.path}", :offset => "#{(milliseconds/1000).to_s}"
+  end
 end
