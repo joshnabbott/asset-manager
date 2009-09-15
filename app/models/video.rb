0a3b8d4892900b5809d1073435c06995f55c6c7a
quest <quest@questmbp.local>
2009-09-15 16:57:00 -0700

    Video basics completed

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,8 @@ require 'rvideo'
 
 class Video < Asset
   has_attached_file :file, :url => "/system/videos/:attachment/:id/:style/:filename"
-  has_attached_file :preview, :url => "/system/videos/:attachment/:id/:style/:filename"
+  has_attached_file :preview, :url => "/system/videos/:attachment/:id/:style/:filename", :styles => {:icon => "100x100#", :master => "500x500>"}
+  validates_presence_of :title
   validates_attachment_presence :file
   validates_attachment_content_type :file, :content_type => /video/
   
@@ -13,5 +14,7 @@ class Video < Asset
     ENV['PATH'] = '/usr/local/bin:' + ENV['PATH']
     frame = RVideo::FrameCapturer.capture! :input => "#{self.file.queued_for_write[:original].path}", :offset => "#{(milliseconds/1000).to_s}"
     self.preview = File.open(frame[0], 'r')
+    self.height = Paperclip::Geometry.from_file(self.preview.queued_for_write[:original].path).try(:height)
+    self.width = Paperclip::Geometry.from_file(self.preview.queued_for_write[:original].path).try(:width)
   end
 end
