58969b96d6e7ab8042f2fabc15a11a21956309d9
Josh N. Abbott <joshnabbott@gmail.com>
2009-09-16 11:01:50 -0700

    merge commits

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,14 +1,4 @@
 class Asset < ActiveRecord::Base
   belongs_to :owner
-<<<<<<< HEAD:app/models/asset.rb
-end
-=======
   acts_as_taggable_on :tags
-
-  # Ruby method to call system identify. Accepts regular arguments of `identify`
-  # Run identify -help in a terminal window to see available options
-  def identify(image, *args)
-    `identify #{args.join(' ')} #{image}`
-  end
 end
->>>>>>> origin/videos:app/models/asset.rb
