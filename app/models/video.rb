7410fe3a30da3af0096f80eb8c2ad31956187ece
quest <quest@questmbp.local>
2009-09-17 15:12:05 -0700

    Removed a validation

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,7 +6,6 @@ class Video < Asset
   has_attached_file :preview, :url => "/system/videos/:attachment/:id/:style/:filename", :styles => {:icon => "100x100>", :master => "500x500>"}
   validates_attachment_presence :file
   validates_attachment_content_type :file, :content_type => /video/
-  validates_presence_of :title
   
   before_file_post_process :create_preview
   
